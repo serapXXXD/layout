61b43f1933fbce51832593cbde2954599f58329b
serapXXXD <serapXXXD@gmail.com>
2023-09-05 21:54:15 +0300

    correct buttons: share, likes and comments

--- static/js/main.js
+++ static/js/main.js
@@ -1,37 +1,45 @@
 const textArea = document.getElementById('addCommentText')
 textArea.addEventListener('focusin', () => {
-    textArea.setAttribute('rows', 8)
+  textArea.setAttribute('rows', 8)
 })
 textArea.addEventListener('focusout', () => {
-    textArea.setAttribute('rows', 1)
+  textArea.setAttribute('rows', 1)
 })
 
 
 
-function toggleBlock (blockName) {
+function toggleBlock(blockName) {
   const likeBlock = document.getElementById('likeBlock');
-const commentsBlock = document.getElementById('commentsBlock');
-const shareBlock = document.getElementById('shareBlock');
-  const elements = [likeBlock, commentsBlock, shareBlock];
+  const commentsBlock = document.getElementById('commentsBlock');
+  const shareBlock = document.getElementById('shareBlock');
+  const likeBtn = document.getElementById('likeBtn');
+  const commentsBtn = document.getElementById('commentsBtn');
+  const shareBtn = document.getElementById('shareBtn');
+  const elements = [[likeBlock, likeBtn], [commentsBlock, commentsBtn], [shareBlock, shareBtn]];
   elements.forEach((el) => {
-    console.log(el);
-    if (!el.classList.contains('hidden')) {
-      if (el.id !== `${blockName}Block`) {
-        el.classList.add('hidden');
+    // console.log(el);
+    if (!el[0].classList.contains('hidden')) {
+      if (el[0].id !== `${blockName}Block`) {
+        el[0].classList.add('hidden');
+        el[1].classList.remove(`button__${blockName}__pressed`);
       }
-      
+
     }
   }
   )
   const element = document.getElementById(`${blockName}Block`);
+  const button = document.getElementById(`${blockName}Btn`);
+  button.classList.toggle(`button__${blockName}__pressed`);
   element.classList.toggle('hidden');
 }
 
-  
-  // const footerController = new FooterController();
-  const likeBtn = document.getElementById('likeBtn');
-  const commentsBtn = document.getElementById('commentsBtn');
-  const shareBtn = document.getElementById('shareBtn');
-  likeBtn.addEventListener('click', ()=>{toggleBlock('like')});
-  commentsBtn.addEventListener('click', ()=>{toggleBlock('comments')} );
-  shareBtn.addEventListener('click', ()=>{toggleBlock('share')});
+
+// const footerController = new FooterController();
+const likeBtn = document.getElementById('likeBtn');
+const commentsBtn = document.getElementById('commentsBtn');
+const shareBtn = document.getElementById('shareBtn');
+likeBtn.addEventListener('click', () => { toggleBlock('like') });
+commentsBtn.addEventListener('click', () => { toggleBlock('comments') });
+shareBtn.addEventListener('click', () => { toggleBlock('share') });
+
+
